de22c348464778a973878f1c147d405a2f8406d2
BuildTools <unconfigured@null.spigotmc.org>
2022-11-27 09:40:10 -0500

    Database connected

---
+++ SpringBoot/com.inn.rotonda/src/main/java/com/inn/rotonda/RotondaComidaApplication.java
@@ -0,0 +1,13 @@
+package com.inn.rotonda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotondaComidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RotondaComidaApplication.class, args);
+	}
+
+}
